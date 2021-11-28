1e50319a4f043d0475444f5868d3dfd213fd8746
Simon Livingston <simonblivingston@gmail.com>
2021-11-28 13:00:41 -0500

    Initial Commit

---
+++ Learning GitHub/Learning_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GitHubApp.swift
+//  Learning GitHub
+//
+//  Created by Simon Livingston on 28/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
